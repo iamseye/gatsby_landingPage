c8f207188fb341dda7f68a61be1a7b1152ceb827
Jess <iamseye@gmail.com>
2019-04-30 10:40:25 +0800

    ADD: intro section

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React from 'react';
 
-import { COLORS, GRADIENT } from "../styles/constants"
-import "../styles/button.scss"
+import { COLORS, GRADIENT } from '../styles/constants';
+import '../styles/button.scss';
 
 const Button = ({ children }) => (
   <button
+    type="button"
     style={{
       color: COLORS.lightWhite,
       background: GRADIENT,
@@ -12,6 +13,6 @@ const Button = ({ children }) => (
   >
     {children}
   </button>
-)
+);
 
-export default Button
+export default Button;
--- src/components/intro.js
+++ src/components/intro.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import logo from '../images/logo.svg'
-import "../styles/intro.scss"
+import React from 'react';
+import logo from '../images/logo.svg';
+import imageIntro from '../images/illustration-hero.svg';
+import '../styles/intro.scss';
 
 const Intro = () => (
   <section className="intro-section">
     <header>
       <img src={logo} alt="Logo" />
-      <div></div>
+      <div>
+        <button type="button" className="btn-trans">Sign in</button>
+        <button type="button" className="btn-green">Sign up</button>
+      </div>
     </header>
-    <div>
+
+    <div className="intro-content">
+      <div className="intro-content--left">
+        <h1 style={{ marginBottom: '1rem' }}> All Your Files In One Secure </h1>
+        <h1> Location, Accessible Anywhere.</h1>
+        <button style={{ marginTop: '1rem' }} type="button" className="btn-green--big">Get Started</button>
+      </div>
+      <img src={imageIntro} alt="illustration hero" />
     </div>
   </section>
-)
+);
 
-export default Intro
+export default Intro;
